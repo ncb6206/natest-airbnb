c100ceed20cd80130ac9567a944a399a21b38454
ncb6206 <ncb6206@gmail.com>
2023-10-25 04:37:43 +0900

    Feat : Navbar UI 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 import './globals.css';
 import { ReactNode } from 'react';
+import Navbar from './components/navbar/Navbar';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
